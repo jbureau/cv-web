b693d592b50ca6e796f48d9163bb7bf83cafb6f9
Julie Bureau <bureau.julie@ymail.com>
2018-01-16 12:16:57 +1300

    modif main text

--- js/const.js
+++ js/const.js
@@ -6,9 +6,7 @@ export const USER_NAME = `Olivier Bureau`;
 export const EXPERIENCES_TITLE = `Experiences`;
 
 /* TODO get from back */
-export const USER_DESC = `Mon  profil d’origine  technique,  a  évolué  peu  à  peu  vers  l'expertise,  grâce  à  
-    un  début  de  carrière en  milieu fortement  normalisé  (Contrôle  Qualité  aéronautique),  et  une  évolution  progressive  
-    vers  les  systèmes d’informations  multi-métiers. Dans ce cadre-là, SAP fut le catalyseur de mon nouvel 
-    élan professionnel vers le Conseil  informatique.  Depuis  1999,  mes  interventions  en  clientèle  
-    m'identifient  comme  le  spécialiste  des domaines industriels transverses, dans lesquels je soigne 
-    particulièrement l’approche "Métier"`;
+export const USER_DESC = `<strong>Bienvenue sur mon CV interactif.</strong><br />
+Frustré de devoir laisser aux recruteurs, un CV classique sur lequel je n’ai plus assez de place pour m’exprimer, je propose de compléter ce dernier par quelques clics à 
+votre bon vouloir. Vous y retrouverez le détail de mes missions SAP, mais aussi ce que j’en ai retenu. Merci à ma fille Julie pour la réalisation de ce CV interactif, 
+et pour avoir répondu au besoin d’un « Papa–client » particulièrement exigeant !`;
